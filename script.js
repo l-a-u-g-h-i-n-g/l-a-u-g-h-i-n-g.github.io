61bbfcdaf7c7e256ee6151cab7e40ff9030ddb25
l-a-u-g-h-i-n-g <laughingatyou10pm@duck.com>
2025-02-11 23:56:20 -0700

    e

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += " responsive";
+    x.className += " responsive"; // Open the sidebar
   } else {
-    x.className = "topnav";
+    x.className = "topnav"; // Close the sidebar
   }
 }
